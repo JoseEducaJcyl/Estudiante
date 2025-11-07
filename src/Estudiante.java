f44cedfe3d623291e00bf56ff8781b6157978a17
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2025-11-07 09:24:26 +0100

    Cuarto ejercicio del bloque 2 (POO)

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
-
+//Clase Estudiante con los atributos nombre, edad y nota
 public class Estudiante {
+    private String nombre;
+    private int edad;
+    private int nota;
+
+    //Declaramos los constructores
+    public Estudiante() {}
+
+    public Estudiante(String nombre, int edad, int nota) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nota = nota;
+    }
+
+    //Declaracion de setters y getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    //En setNota() se hace una restriccion para que la nota sea mayoro igual que 0
+    //y menor o igual a 10
+    public void setNota(int nota) {
+        if(nota >= 0 && nota <= 10){
+            this.nota = nota;
+        }else {
+            System.out.println("El nota debe ser mayor o igual a 10");
+        }
+    }
+
+    //Metodo toString para mostrar los atributos
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "Nombre='" + nombre + '\'' +
+                ", Edad=" + edad +
+                ", Nota=" + nota +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+// Clase Main con el metodo main para ejecutar el codigo
+public class Main {
+    public static void main(String[] args) {
+        //Declaramos dos objetos de la clase Estudiante, estudiante usando el constructor con sus atributos
+        //y estudiante2 con un constructor vacio
+        Estudiante estudiante = new Estudiante("nombre1",18,5);
+        Estudiante estudiante2 = new Estudiante();
+        //Declaramos los atributos del objeto estudiante2
+        //Asegurandonos que funciona la restriccion en setNota()
+        estudiante2.setNombre("nombre2");
+        estudiante2.setEdad(25);
+        estudiante2.setNota(-1);
+        estudiante2.setNota(0);
+        System.out.println(estudiante.toString());
+        System.out.println(estudiante2.toString());
+    }
 }
